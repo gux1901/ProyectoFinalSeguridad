6cb67cd27a2aff4c2124a4b913f3e547a6c46308
Gustavo Garcia <gux1901@gmail.com>
2023-11-24 19:13:15 -0600

    Agregando informacion

---
+++ ProyectoFinalSeguridad/ProyectoFinalSeguridad/Models/DatosEnc.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoFinalSeguridad.Models
+{
+    public class DatosEnc
+    {
+        private byte[] key;
+        private byte[] iv;
+        private string texto;
+
+        public byte[] Key { get => key; set => key = value; }
+        public byte[] Iv { get => iv; set => iv = value; }
+        public string Texto { get => texto; set => texto = value; }
+    }
+}
---
+++ ProyectoFinalSeguridad/ProyectoFinalSeguridad/Models/Encriptador.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoFinalSeguridad.Models
+{
+    public class Encriptador
+    {
+        private string plainText;
+        private byte[] key;
+        private byte[] iV;
+
+        public string PlainText { get => plainText; set => plainText = value; }
+        public byte[] Key { get => key; set => key = value; }
+        public byte[] IV { get => iV; set => iV = value; }
+    }
+}
