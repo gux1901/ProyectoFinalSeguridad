4ad7ffa6cba0b9619b373d11aad7ed5902fccbef
gux1901 <142961694+gux1901@users.noreply.github.com>
2023-11-29 12:17:26 -0600

    Add files via upload

---
+++ InterfazSeguridad/ValidarTarjeta.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace InterfazSeguridad
+{
+    partial class ValidarTarjeta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.lblTarjeta = new System.Windows.Forms.Label();
+            this.txbTarjeta = new System.Windows.Forms.TextBox();
+            this.lblProcesando = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(12, 73);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 0;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // lblTarjeta
+            // 
+            this.lblTarjeta.AutoSize = true;
+            this.lblTarjeta.Location = new System.Drawing.Point(22, 34);
+            this.lblTarjeta.Name = "lblTarjeta";
+            this.lblTarjeta.Size = new System.Drawing.Size(40, 13);
+            this.lblTarjeta.TabIndex = 1;
+            this.lblTarjeta.Text = "Tarjeta";
+            // 
+            // txbTarjeta
+            // 
+            this.txbTarjeta.Location = new System.Drawing.Point(96, 34);
+            this.txbTarjeta.Name = "txbTarjeta";
+            this.txbTarjeta.Size = new System.Drawing.Size(157, 20);
+            this.txbTarjeta.TabIndex = 2;
+            // 
+            // lblProcesando
+            // 
+            this.lblProcesando.AutoSize = true;
+            this.lblProcesando.Location = new System.Drawing.Point(14, 122);
+            this.lblProcesando.Name = "lblProcesando";
+            this.lblProcesando.Size = new System.Drawing.Size(73, 13);
+            this.lblProcesando.TabIndex = 3;
+            this.lblProcesando.Text = "Procesando...";
+            // 
+            // ValidarTarjeta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 450);
+            this.Controls.Add(this.lblProcesando);
+            this.Controls.Add(this.txbTarjeta);
+            this.Controls.Add(this.lblTarjeta);
+            this.Controls.Add(this.btnProcesar);
+            this.Name = "ValidarTarjeta";
+            this.Text = "ValidarTarjeta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Label lblTarjeta;
+        private System.Windows.Forms.TextBox txbTarjeta;
+        private System.Windows.Forms.Label lblProcesando;
+    }
+}
---
+++ InterfazSeguridad/ValidarTarjeta.cs
@@ -0,0 +1,79 @@
+﻿using InterfazSeguridad.wsSeguridad;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfazSeguridad
+{
+    public partial class ValidarTarjeta : Form
+    {
+        public static byte[] generateKey;
+        public static byte[] generateIv;
+        public static string tarjeta;
+        public static byte[] encryptText;
+        public static byte[] hash;
+        public static byte[] hashDesencriptado;
+        public static string decryptText;
+        public static readonly wsSeguridad.WsSeguridad ws = new wsSeguridad.WsSeguridad();
+        public ValidarTarjeta()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            tarjeta = Regex.Replace(txbTarjeta.Text, @"[^\w\s.!@$%^&*()\-\/]+", "");
+            lblProcesando.Text = "Texto Sanitizado "+ tarjeta;
+
+            string textoSanitizado = ws.Tarjetas(tarjeta);
+            lblProcesando.Text += "\nValidacion Tarjeta "+ textoSanitizado;
+            if (!textoSanitizado.Contains("No es tarjeta Valida"))
+            {
+                if (generateIv == null && generateKey == null)
+                {
+                    GeneradorLlaves llaves = new GeneradorLlaves();
+                    llaves = ws.ObtenerLlaves();
+                    generateKey = llaves.Key;
+                    generateIv = llaves.Iv;
+                }
+                lblProcesando.Text += "\nLlave: " + Convert.ToBase64String(generateKey);
+                lblProcesando.Text += "\nIV: " + Convert.ToBase64String(generateIv);
+                hash = ws.Hashing(tarjeta);
+                lblProcesando.Text += "\nHash: " + Convert.ToBase64String(hash);
+                wsSeguridad.Encriptador objetoEncriptador = new Encriptador();
+                
+                objetoEncriptador.Key = generateKey;
+                objetoEncriptador.IV = generateIv;
+                objetoEncriptador.PlainText = tarjeta;
+                encryptText = ws.Encriptar(objetoEncriptador);
+
+                lblProcesando.Text += "\nTextoEncriptado: " + Convert.ToBase64String(encryptText);
+
+                Desencriptador desencriptador = new Desencriptador();
+                desencriptador.Key = generateKey;
+                desencriptador.IV = generateIv;
+                desencriptador.CipherText = encryptText;
+                decryptText = ws.Desencriptar(desencriptador);
+
+                lblProcesando.Text += "\nTextoDesencriptado: " + decryptText;
+
+                hashDesencriptado = ws.Hashing(decryptText);
+                lblProcesando.Text += "\nHash despues de desencriptar: " + decryptText;
+
+                if (Convert.ToBase64String(hash) == Convert.ToBase64String(hashDesencriptado)) lblProcesando.Text += "\nPrimer Hash igual a hash despues de desencriptacion ";
+            }
+
+
+
+
+
+        }
+    }
+}
